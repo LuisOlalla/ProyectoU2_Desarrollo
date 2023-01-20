f1149e0feb29c82c87ceb32bccec89e610d3c7ab
Selena Rivas <75510255+Selena26@users.noreply.github.com>
2023-01-20 07:57:29 -0500

    Controlador de shopping

---
+++ ShoppingController.cs
@@ -0,0 +1,134 @@
+﻿using ProyectoU2.Models;
+using ProyectoU2.Permisos;
+using ProyectoU2.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace ProyectoU2.Controllers
+{
+    [ValidarSesion]
+    public class ShoppingController : Controller
+    {
+        private EcartDBEntities ecartDBEntities;
+        private List<ShoppingCartModel> listOfShoppingCartModels;
+
+        public ShoppingController()
+        {
+            ecartDBEntities = new EcartDBEntities();
+            listOfShoppingCartModels = new List<ShoppingCartModel>();
+        }
+        // GET: Shopping
+        public ActionResult Index()
+        {
+            IEnumerable<ShoppingViewModel> listOfShoppingViewModels = (from objItem in ecartDBEntities.Items
+                                                                       join
+                                                                           objCate in ecartDBEntities.Categories
+                                                                           on objItem.CategoryId equals objCate.CategoryId
+                                                                       select new ShoppingViewModel()
+                                                                       {
+                                                                           ImagePath = objItem.ImagePath,
+                                                                           ItemName = objItem.ItemName,
+                                                                           Description = objItem.Description,
+                                                                           ItemPrice = objItem.ItemPrice,
+                                                                           ItemId = objItem.ItemId,
+                                                                           Category = objCate.CategoryName,
+                                                                           ItemCode = objItem.ItemCode
+                                                                       }
+
+                ).ToList();
+            return View(listOfShoppingViewModels);
+
+
+        }
+
+        [HttpPost]
+        public JsonResult Index(string ItemId)
+
+        {
+
+            ShoppingCartModel objShoppingCartModel = new ShoppingCartModel();
+
+            Items objItem = ecartDBEntities.Items.Single(model => model.ItemId.ToString() == ItemId);
+            if (Session["CartCounter"] != null)
+            {
+                listOfShoppingCartModels = Session["CartItem"] as List<ShoppingCartModel>;
+
+            }
+            if (listOfShoppingCartModels.Any(model => model.ItemId == ItemId))
+            {
+                objShoppingCartModel = listOfShoppingCartModels.Single(model => model.ItemId == ItemId);
+                objShoppingCartModel.Quantity = objShoppingCartModel.Quantity + 1;
+                objShoppingCartModel.Total = objShoppingCartModel.Quantity + objShoppingCartModel.UnitPrice;
+
+
+
+            }
+            else
+            {
+                objShoppingCartModel.ItemId = ItemId;
+                objShoppingCartModel.ImagePath = objItem.ImagePath;
+                objShoppingCartModel.ItemName = objItem.ItemName;
+                objShoppingCartModel.Quantity = 1;
+                objShoppingCartModel.Total = objItem.ItemPrice;
+                objShoppingCartModel.UnitPrice = objItem.ItemPrice;
+                listOfShoppingCartModels.Add(objShoppingCartModel);
+
+
+
+            }
+            Session["CartCounter"] = listOfShoppingCartModels.Count;
+            Session["CartItem"] = listOfShoppingCartModels;
+            return Json(new { Success = true, Counter = listOfShoppingCartModels.Count }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult ShoppingCart()
+        {
+            listOfShoppingCartModels = Session["CartItem"] as List<ShoppingCartModel>;
+            return View(listOfShoppingCartModels);
+        }
+        [HttpPost]
+        public ActionResult AddOrder()
+        {
+            int OrderId = 0;
+            listOfShoppingCartModels = Session["CartItem"] as List<ShoppingCartModel>;
+            Orders orderObj = new Orders()
+            {
+                OrderDate = DateTime.Now,
+                OrderNumber = String.Format("{0:ddmmyyyyHHmmsss}", DateTime.Now)
+            };
+            ecartDBEntities.Orders.Add(orderObj);
+            ecartDBEntities.SaveChanges();
+            OrderId = orderObj.OrderId;
+
+            foreach (var item in listOfShoppingCartModels)
+            {
+                OrderDetails objOrderDetail = new OrderDetails();
+                objOrderDetail.Total = item.Total;
+                objOrderDetail.ItemId = item.ItemId;
+                objOrderDetail.OrderId = OrderId;
+                objOrderDetail.Quantity = item.Quantity;
+                objOrderDetail.UnitPrice = item.UnitPrice;
+                ecartDBEntities.OrderDetails.Add(objOrderDetail);
+                ecartDBEntities.SaveChanges();
+            }
+            Session["CartItem"] = null;
+            Session["CartCounter"] = null;
+            return RedirectToAction("Index");
+        }
+        public ActionResult Eliminar(int itemId)
+        {
+            using (EcartDBEntities db = new EcartDBEntities())
+            {
+                var objOrderDetail = db.OrderDetails.Find(itemId);
+                db.OrderDetails.Remove(objOrderDetail);
+                db.SaveChanges();
+            }
+            return Redirect("~/Shopping/");
+        }
+
+    }
+}
